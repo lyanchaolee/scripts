e238c1b75eb101f1bb3972aacbb7fbc75930c077
lyc <yanchao.liyc@alibaba-inc.com>
2013-07-01 18:19:48 +0800

    "final test case"

--- java/runprogram.sh
+++ java/runprogram.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-java -cp /Users/lyc/Desktop/testcase/testcase.jar com.alibaba.test.RunDNASortingTest $*
+java -cp /Users/lyc/Desktop/testcase/testcase.jar:/Users/lyc/Desktop/testcase/commons-lang-2.4.jar com.alibaba.test.RunDNASortingTest $*
--- java/src/com/alibaba/test/RunDNASortingTest.java
+++ java/src/com/alibaba/test/RunDNASortingTest.java
@@ -1,21 +1,24 @@
 package com.alibaba.test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+
+import org.apache.commons.lang.StringUtils;
 
 public class RunDNASortingTest {
 
     private static final String BLANK                = " ";
 
-    private static final String TEN_SIX_CASE         = "AACATGAAGG TTTTGGCCAA TTTGGCCAAA GATCAGATTT CCCGGGGGGA ATCGATGCAT";
-    private static final String TEN_SIX_RESULT       = "TEST1 TEST2";
+    private static final String TEN_SIX_CASE         = "com/alibaba/test/106.testcase";
+    private static final String TEN_SIX_RESULT       = "com/alibaba/test/106.result";
 
-    private static final String TWENTY_FIFTY_CASE    = "AACATGAAGG TTTTGGCCAA TTTGGCCAAA GATCAGATTT CCCGGGGGGA ATCGATGCAT";
-    private static final String TWENTY_FIFTY_RESULT  = "TEST1 TEST2";
+    private static final String TEN_TEN_CASE         = "com/alibaba/test/1010.testcase";
+    private static final String TEN_TEN_RESULT       = "com/alibaba/test/1010.result";
 
-    private static final String FIFTY_HUNDRED_CASE   = "AACATGAAGG TTTTGGCCAA TTTGGCCAAA GATCAGATTT CCCGGGGGGA ATCGATGCAT";
-    private static final String FIFTY_HUNDRED_RESULT = "TEST1 TEST2";
+    private static final String FIFTY_HUNDRED_CASE   = "com/alibaba/test/50100.testcase";
+    private static final String FIFTY_HUNDRED_RESULT = "com/alibaba/test/50100.result";
 
     public static void main(String[] args) throws IOException, InterruptedException {
         if (args == null || (args.length != 2 && args.length != 4)) {
@@ -34,23 +37,49 @@ public class RunDNASortingTest {
 
     private static void callAndMeasure(String programCommand, String programName) throws IOException,
                                                                                  InterruptedException {
-        String tenSixResult = exe(programCommand, programName, TEN_SIX_CASE);
-        checkResult(TEN_SIX_CASE, tenSixResult, TEN_SIX_RESULT);
-        String twentyFiftyResult = exe(programCommand, programName, TWENTY_FIFTY_CASE);
-        checkResult(TWENTY_FIFTY_CASE, twentyFiftyResult, TWENTY_FIFTY_RESULT);
+        String tenSixResult = exe(programCommand, programName, getStringFromFile(TEN_SIX_CASE));
+        checkResult(TEN_SIX_CASE, tenSixResult, getStringFromFile(TEN_SIX_RESULT));
+        String twentyFiftyResult = exe(programCommand, programName, getStringFromFile(TEN_TEN_CASE));
+        checkResult(TEN_TEN_CASE, twentyFiftyResult, getStringFromFile(TEN_TEN_RESULT));
 
         long timeBegin = System.currentTimeMillis();
-        String fiftyHundredResult = exe(programCommand, programName, FIFTY_HUNDRED_CASE);
-        checkResult(FIFTY_HUNDRED_CASE, fiftyHundredResult, FIFTY_HUNDRED_RESULT);
+        String fiftyHundredResult = exe(programCommand, programName, getStringFromFile(FIFTY_HUNDRED_CASE));
+        checkResult(FIFTY_HUNDRED_CASE, fiftyHundredResult, getStringFromFile(FIFTY_HUNDRED_RESULT));
         System.out.println("sucess and cost in mili seconds: " + (System.currentTimeMillis() - timeBegin));
     }
 
-    private static void checkResult(String input, String tenSixResult, String tenSixResult2) {
-        if (tenSixResult.trim().equals(tenSixResult2.trim())) {
+    private static String getStringFromFile(String result) throws IOException {
+        InputStream in = Thread.currentThread().getContextClassLoader().getSystemResource(result).openStream();
 
-        } else {
+        LineNumberReader numberBuffer = new LineNumberReader(new InputStreamReader(in));
+
+        StringBuilder builder = new StringBuilder();
+        String input;
+        while ((input = numberBuffer.readLine()) != null) {
+            builder.append(input.trim()).append(BLANK);
+        }
+        return builder.toString().trim();
+    }
+
+    private static void checkResult(String input, String tenSixResult, String tenSixResult2) {
+
+        String result[] = StringUtils.split(tenSixResult, BLANK);
+        String result2[] = StringUtils.split(tenSixResult2, BLANK);
+
+        if (result.length != result2.length) {
+            System.out.println(result.length + " : " + result2.length);
             throw new RuntimeException("result not right in the case of : " + input + " , the result is: "
                                        + tenSixResult);
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i].trim().equals(result2[i].trim())) {
+
+            } else {
+                System.out.println("index " + i + "not match : " + result[i] + " " + result2[i]);
+                throw new RuntimeException("result not right in the case of : " + input + " , the result is: "
+                                           + tenSixResult);
+            }
         }
 
     }
@@ -80,6 +109,6 @@ public class RunDNASortingTest {
             resultInfoBuilder.append(resultInfo.trim()).append(BLANK);
         }
 
-        return resultInfoBuilder.toString();
+        return resultInfoBuilder.toString().trim();
     }
 }
