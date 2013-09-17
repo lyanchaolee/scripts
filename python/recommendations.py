72ab6d5b19ba52bf1e64cb16269436dbb9eea166
lyc <yanchao.liyc@alibaba-inc.com>
2013-09-17 19:45:32 +0800

    xxx

--- python/recommendations.py
+++ python/recommendations.py
@@ -42,4 +42,62 @@ for k1 in critics.keys():
 			sortset.append(keyappend)
 			print 'score between', k1, k2, 'is', sim_distance(k1, k2)
 
+from math import sqrt
+
+def sim_pearson(p1, p2,prefs=critics):
+	si = {}
+	for item in prefs[p1]:
+		if item in prefs[p2]:
+			si[item]=1
+
+	n = len(si)
+	if n==0:
+		return 0
+
+	sum1 = sum([prefs[p1][item] for item in si])
+	sum2 = sum([prefs[p2][item] for item in si])
+
+	sum1Sq = sum([pow(prefs[p1][item],2) for item in si])
+	sum2Sq = sum([pow(prefs[p2][item],2) for item in si])
+
+	pSum = sum([prefs[p1][item]*prefs[p2][item] for item in si])
+
+	num = pSum - (sum1*sum2/n)
+
+	den = sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2, 2)/n))
+
+	if den==0:
+		return 0
+
+	return num/den
+
+print '**********'*10
+
+sortset=[]
+for k1 in critics.keys():
+	for k2 in critics.keys():
+		if k1!=k2:
+			keyappend = k1+k2 if k1>k2 else k2+k1
+			if keyappend in sortset:
+				continue
+			sortset.append(keyappend)
+			print 'pearson between', k1, k2, 'is', sim_pearson(k1, k2)
+
+
+def topNpearson(p1, n=5, prefs=critics):
+	sortset = [(sim_pearson(p1, other,prefs),other) for other in prefs if other!=p1]
+	sortset.sort()
+	sortset.reverse()
+	return sortset[0:n]
+
+print '**********'*10
+
+print 'TOP 3 Gene Seymour are : ', topNpearson('Gene Seymour',3)
+
+
+	
+
+
+
+
 
